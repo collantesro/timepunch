0088df7b205b061cccc5b57e4b44676c3552eddf
Kion <kionkshamsa@gmail.com>
2018-03-28 21:04:25 -0600

    Made a test SQL Statement

--- Methods/DataAccess.cs
+++ Methods/DataAccess.cs
@@ -1,7 +1,39 @@
 using Microsoft.Data.Sqlite;
+using System.Collections.Generic; 
+using System; 
 public class Methods {
     
 #region Gets
+
+public List<string> GetUsers()  {
+
+    List<string> users = new List<string>(); 
+
+    using(SqliteConnection connect = new SqliteConnection("")) {
+        connect.Open(); 
+        using(SqliteCommand qry = connect.CreateCommand()) {
+            qry.CommandText = @"SELECT * FROM Users"; 
+            //qry.CommandType = CommandType.Text
+            SqliteDataReader r = qry.ExecuteReader(); 
+
+            while(r.Read()) {
+                //ImportedFiles.Add(Convert.ToString(r["FileName"]));
+                
+                //User u = new User(); 
+                //u.name = (string)r["FileName"];
+
+
+                //users.add(u); 
+            }
+        }
+    }
+
+    if(users.Count == 0) {
+        throw new Exception("List is empty");
+    } else {
+        return(users);
+    }
+}
 
 #endregion
 
