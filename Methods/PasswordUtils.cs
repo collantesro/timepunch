7a0065a568ff896c44bce2cc633ca9615b9be331
Rolando Collantes <rolandocollantes@mail.weber.edu>
2018-04-12 00:31:44 -0600

    Revert "Fix indentation in PasswordUtils"
    This reverts commit 182bf5f452f1b7d978f6bbcd04e0e3cb4bf97ee0.

--- Methods/PasswordUtils.cs
+++ Methods/PasswordUtils.cs
@@ -2,42 +2,41 @@
 using System.Security.Cryptography;
 using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 
-namespace timepunch
+namespace timepunch{
+public class PasswordUtils
 {
-    public class PasswordUtils
+    public static byte[] generateSalt()
     {
-        public static byte[] generateSalt()
+        byte[] salt = new byte[16];
+        using (var rng = RandomNumberGenerator.Create())
         {
-            byte[] salt = new byte[16];
-            using (var rng = RandomNumberGenerator.Create())
-            {
-                rng.GetBytes(salt);
-            }
-            Console.WriteLine($"Salt: {Convert.ToBase64String(salt)}");
-            return salt;
+            rng.GetBytes(salt);
         }
-        public static string passwordEncrypt(byte[] salt, string password)
+        Console.WriteLine($"Salt: {Convert.ToBase64String(salt)}");
+        return salt;
+    }
+    public static string passwordEncrypt(byte[] salt, string password)
+    {
+        string savedHash = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+            password: password,
+            salt: salt,
+            prf: KeyDerivationPrf.HMACSHA1,
+            iterationCount: 10000,
+            numBytesRequested: 32));
+        Console.WriteLine($"Hashed: {savedHash}");
+        return savedHash;
+    }
+    public static bool checkPassword(string storedHash, byte[] salt, string password)
+    {
+        string passedInHash = passwordEncrypt(salt, password);
+        if (storedHash == passedInHash)
         {
-            string savedHash = Convert.ToBase64String(KeyDerivation.Pbkdf2(
-                password: password,
-                salt: salt,
-                prf: KeyDerivationPrf.HMACSHA1,
-                iterationCount: 10000,
-                numBytesRequested: 32));
-            Console.WriteLine($"Hashed: {savedHash}");
-            return savedHash;
+            return true;
         }
-        public static bool checkPassword(string storedHash, byte[] salt, string password)
+        else
         {
-            string passedInHash = passwordEncrypt(salt, password);
-            if (storedHash == passedInHash)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            return false;
         }
     }
+}
 }
