0fc00abc345ee5554044dd37810531525cf16793
Rolando Collantes <rolandocollantes@mail.weber.edu>
2018-04-11 20:40:30 -0600

    Emptied out the Index page, created blank Login and CreateAccount razor pages

--- Pages/CreateAccount.cshtml.cs
+++ Pages/CreateAccount.cshtml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,11 +7,11 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace timepunch.Pages
 {
-    public class IndexModel : PageModel
+    public class CreateAccountModel : PageModel
     {
         public void OnGet()
         {
-            
+
         }
     }
 }
