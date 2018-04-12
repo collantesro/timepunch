a408e971a641e6224c7c38302df739da04ee600c
Rolando Collantes <rolandocollantes@mail.weber.edu>
2018-04-12 00:27:15 -0600

    Add Matt's passwordutils

---
+++ Methods/PasswordUtils.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
+namespace timepunch{
+public class PasswordUtils
+{
+    public static byte[] generateSalt()
+    {
+        byte[] salt = new byte[16];
+        using (var rng = RandomNumberGenerator.Create())
+        {
+            rng.GetBytes(salt);
+        }
+        Console.WriteLine($"Salt: {Convert.ToBase64String(salt)}");
+        return salt;
+    }
+    public static string passwordEncrypt(byte[] salt, string password)
+    {
+        string savedHash = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+            password: password,
+            salt: salt,
+            prf: KeyDerivationPrf.HMACSHA1,
+            iterationCount: 10000,
+            numBytesRequested: 32));
+        Console.WriteLine($"Hashed: {savedHash}");
+        return savedHash;
+    }
+    public static bool checkPassword(string storedHash, byte[] salt, string password)
+    {
+        string passedInHash = passwordEncrypt(salt, password);
+        if (storedHash == passedInHash)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+}
